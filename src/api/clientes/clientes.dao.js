93881ad1884730d8d5ae70a0f3edd5abd9427b42
André Borges <hoilut@hotmail.com>
2019-12-26 18:38:01 -0300

    Update - Validate cpf/email unique for Clientes

--- src/api/clientes/clientes.dao.js
+++ src/api/clientes/clientes.dao.js
@@ -1,28 +1,47 @@
 import { instances } from 'hapi-sequelizejs';
 import { getObjectOr404 } from '../utils/database.utils';
+import Boom from '@hapi/boom';
 
 export default class ClientesDAO {
-    model = instances.getModel('cliente');
-  
-    async findAll(where) {
-        return this.model.findAll({ where });
-    }
+  model = instances.getModel('cliente');
 
-    async findByID(id) {
-        return await getObjectOr404(this.model, { where: { id } });
-    }
+  async findAll(where) {
+    return this.model.findAll({ where });
+  }
 
-    async create(data) {
-        return await this.model.create(data);
-    }
+  async findByID(id) {
+    return await getObjectOr404(this.model, { where: { id } });
+  }
 
-    async update(id, data) {
-        const cliente = await this.findByID(id);
-        return await cliente.update(data);
-    }
+  async create(data) {
+    const { email, numeroCpfCnpj } = data;
 
-    async destroy(id) {
-        const cliente = await this.findByID(id);
-        return await cliente.destroy(cliente);
-    }
+    const cpfCnpjExists = await this.model.findOne({
+      where: { numeroCpfCnpj },
+    });
+    if (cpfCnpjExists)
+      throw Boom.notAcceptable(
+        'Já existe um cliente com esse Cpf / Cnpj cadastrado. Por favor cadastre um Cpf / Cnpj diferente.',
+      );
+
+    const emailExists = await this.model.findOne({
+      where: { email },
+    });
+    if (emailExists)
+      throw Boom.notAcceptable(
+        'Já existe um cliente com esse Email cadastrado. Por favor cadastre um Email diferente.',
+      );
+
+    return await this.model.create(data);
+  }
+
+  async update(id, data) {
+    const cliente = await this.findByID(id);
+    return await cliente.update(data);
+  }
+
+  async destroy(id) {
+    const cliente = await this.findByID(id);
+    return await cliente.destroy(cliente);
+  }
 }
