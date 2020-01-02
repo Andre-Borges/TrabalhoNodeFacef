0e0001a9436c13c8c1c93e295aa19d4e79ff4969
André Borges <hoilut@hotmail.com>
2020-01-02 17:19:02 -0300

    Update - Validação Totalizador de Produtos

--- src/api/pedidos/pedidos.business.js
+++ src/api/pedidos/pedidos.business.js
@@ -1,12 +1,11 @@
 import PedidosDAO from './pedidos.dao';
-import ProdutosDAO  from '../produtos/produtos.dao';
+import ProdutosDAO from '../produtos/produtos.dao';
 import Boom from '@hapi/boom';
 
 const pedidosDAO = new PedidosDAO();
 const produtosDAO = new ProdutosDAO();
 
 export default class PedidosBusiness {
-
   async list({ query }) {
     return pedidosDAO.findAll(query);
   }
@@ -18,12 +17,20 @@ export default class PedidosBusiness {
   }
 
   async create({ payload }) {
+    let valorTotalPedido = 0;
+
     for (let product of payload['produtos']) {
       let produto_info = await produtosDAO.findByID(product.id);
-      if(produto_info.dataValues.quantidade < product.quantidade){
-        throw Boom.notAcceptable('Quantidade não disponivel em estoque produto: ' + product.id);
+      if (produto_info.dataValues.quantidade < product.quantidade) {
+        throw Boom.notAcceptable(
+          'Quantidade não disponivel em estoque produto: ' + product.id,
+        );
       }
+      valorTotalPedido =
+        parseFloat(valorTotalPedido) + parseFloat(produto_info.valor);
     }
+
+    payload.valor = valorTotalPedido;
     return pedidosDAO.create(payload);
   }
 
@@ -38,5 +45,4 @@ export default class PedidosBusiness {
 
     return pedidosDAO.destroy(id);
   }
-
 }
